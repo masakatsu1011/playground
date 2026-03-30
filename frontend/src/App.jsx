073a649f2d442adb589cf84e650b84797d06226c
masakatsu1011 <kome.sakana.029@gmail.com>
2026-03-30 23:09:54 +0900

    feat: refactor App component to toggle answer visibility and clean up unused imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-
 import { Button } from './components/ui/Button';
 
-function App() {
-  const handleClick = () => {
-    alert("ボタンが押されました！");
-  };
+
+export const App = () => {
+  const[showAnswer, setShowAnswer] = useState(false);
+  const toggle = () => setShowAnswer(!showAnswer);
+
+  console.log("現在の状態(showAnswer):", showAnswer);
 
   return (
     <div style={{ padding: "30px" }}>
       <h1>ボタンのテスト</h1>
-      {/* 作ったボタンを呼び出す */}
-      <Button label="正解を確認する" onClick={handleClick} />
-      
+
+      <Button 
+        label={showAnswer ? "正解を隠す" : "正解を確認する"}
+        onClick={toggle} />
+
+     {showAnswer && (
+        <div style={{ marginTop: "20px", color: "blue" }}>
+          <p>正解は42です</p>
+        </div>
+      )}
+
+
       <div style={{ marginTop: "10px" }}>
         <Button label="次の問題へ" onClick={() => console.log("Next")} color="#059669" />
       </div>
     </div>
   );
-}
+};
+
+
+
 
 export default App
